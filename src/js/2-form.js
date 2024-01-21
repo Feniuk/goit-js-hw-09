ecc5e2ae349b27983e25e6836ffb54911362d96c
Feniuk <143640539+Feniuk@users.noreply.github.com>
2024-01-21 19:40:55 +0100

    final commit

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,5 +1,40 @@
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const textarea = form.querySelector('textarea');
+
+form.addEventListener('input', () => {
+    const userEmail = form.elements.email.value;
+    const userMessage = form.elements.message.value;
+
+    const obj = {
+        email: userEmail,
+        message: userMessage,
+    };
+
+    saveToLS(STORAGE_KEY, obj);
+});
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const userEmail = form.elements.email.value;
+    const userMessage = form.elements.message.value;
+
+    if (!userEmail || !userMessage) {
+        alert('Будь ласка, заповніть обидва елементи форми.');
+        return;
+    }
+
+    const data = loadFromLS(STORAGE_KEY) || {};
+    console.log(data);
+
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+});
+
 function loadFromLS(key = 'empty') {
-    const data = localStorage-getItem(key); 
+    const data = localStorage.getItem(key); 
     try {
     const result = JSON. parse (data) ;
     return result;
@@ -7,3 +42,23 @@ function loadFromLS(key = 'empty') {
     return data;
     }
 };
+
+function saveToLS(key, value) {
+    const jsonData = JSON. stringify(value);
+    localStorage.setItem(key, jsonData);
+};
+
+function restoreData(){
+    const data = loadFromLS(STORAGE_KEY) || {};
+
+    const trimmedEmail = (data.email || '').trim();
+    const trimmedMessage = (data.message || '').trim(); 
+
+    form.elements.email.value = trimmedEmail;
+    form.elements.message.value = trimmedMessage;
+
+    // form.elements.email.value = data.email || '';
+    // form.elements.message.value = data.message || '';
+};
+
+restoreData();
